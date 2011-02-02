90a439256324cb5e8d103bacb997dbefc5cbe784
Robert Middleswarth <robert@middleswarth.net>
2011-02-01 19:28:22 -0500

    Update to 5.3

--- api/App.php
+++ api/App.php
@@ -1,32 +1,35 @@
 <?php
  
 class Cerb5BlogAttachementsTicketTab extends Extension_TicketTab {
-	function __construct($manifest) {
-		$this->DevblocksExtension($manifest,1);
-	}
- 
 	function showTab() {
 		@$ticket_id = DevblocksPlatform::importGPC($_REQUEST['ticket_id'],'integer',0);
-		$tpl = DevblocksPlatform::getTemplateService();
-		$tpl->cache_lifetime = "0";
 		$tpl_path = dirname(dirname(__FILE__)) . '/templates/';
 		
-//		$tpl->assign('response_uri', 'config/attachments');
+		$tpl = DevblocksPlatform::getTemplateService();
+		//$visit = CerberusApplication::getVisit();
+		
+		//$visit->set(Extension_ConfigTab::POINT, 'attachments');
+		$message_ids = array_keys(DAO_Message::getMessagesByTicket($ticket_id));
+		$comment_ids = array_keys(DAO_Comment::getByContext(CerberusContexts::CONTEXT_TICKET, $ticket_id));
+		$attachment_links = array_merge(
+			DAO_AttachmentLink::getByContextIds(CerberusContexts::CONTEXT_MESSAGE, $message_ids),
+			DAO_AttachmentLink::getByContextIds(CerberusContexts::CONTEXT_COMMENT, $comment_ids)
+		);
 
 		$defaults = new C4_AbstractViewModel();
-		$defaults->class_name = 'View_Attachment';
-		$defaults->name = 'Attachements View';
-		$defaults->id = 'ticket_view_attachements';
+		$defaults->class_name = 'View_AttachmentLink';
+		$defaults->name = 'Attachements';
+		$defaults->id = '_ticket_view_attachements';
 		$defaults->renderLimit = 15;
 
 		$view = C4_AbstractViewLoader::getView($defaults->id, $defaults);
-		$view->addParams(array(
-			SearchFields_Attachment::TICKET_ID => new DevblocksSearchCriteria(SearchFields_Attachment::TICKET_ID,DevblocksSearchCriteria::OPER_EQ,$ticket_id)
-		), true);
 		$view->renderPage = 0;
+		$view->addParams(array(
+			SearchFields_AttachmentLink::GUID => new DevblocksSearchCriteria(SearchFields_AttachmentLink::GUID,'in',array_keys($attachment_links)),
+		), true);
 		
 		C4_AbstractViewLoader::setView($view->id,$view);
-	
+		
 		$tpl->assign('view', $view);
 		
 		$tpl->display('file:' . $tpl_path . 'attachments/index.tpl');
